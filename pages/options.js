5663a3071e4cb0ab1e77d9fd95f6fb3450a5dc0a
codetariat <71725599+codetariat@users.noreply.github.com>
2022-05-13 23:31:51 +0200

    Improved options.js

--- pages/options.js
+++ pages/options.js
@@ -1,45 +1,73 @@
 let bots = []
+let usernameColumn;
+let idColumn;
 
-function addbots(n){
-	// bad code
-	document.getElementById("botcol1").innerHTML=document.getElementById("botcol1").innerHTML+bots[n][0]+"<br>";
-	document.getElementById("botcol2").innerHTML=document.getElementById("botcol2").innerHTML+bots[n][1]+"<br>";
-	if(n<15){
-		setTimeout(function(){addbots(n+1)},100)
-	}else{
-		let rest = ""
-		let restN = ""
-		function add(n){
-			let x = n+15
-			if(n<bots.length-15){
-				rest = rest+bots[x][0]+"<br>"
-				restN = restN+bots[x][1]+"<br>"
-				add(n+1)
-			}else{
-				document.getElementById("botcol1").innerHTML=document.getElementById("botcol1").innerHTML+rest
-				document.getElementById("botcol2").innerHTML=document.getElementById("botcol2").innerHTML+restN
-			}
-		}
-		add(1)
+function addText(element, text, options){
+	// Get element name to create
+	let elementName = options && options.customElement || `span`
+
+	// Create new HTML element newElement
+	let newElement = document.createElement(elementName);
+
+	// Modify text and append newElement to given element
+	newElement.textContent = text;
+	element.appendChild(newElement);
+}
+
+function breakLine(element){
+	// Create br element and append to given element
+	let br = document.createElement(`br`);
+	element.appendChild(br);
+}
+
+function displayBots(){
+	// Add Username and User ID header to columns
+	addText(usernameColumn, `Username`, {customElement: `b`})
+	addText(idColumn, `User ID`, {customElement: `b`})
+
+	// Loop through all bots
+	for(let k in bots){
+		if(k == 0){ continue }
+		let botId = bots[k][0];
+		let botUsername = bots[k][1];
+
+		breakLine(usernameColumn);
+		addText(usernameColumn, botUsername);
+
+		breakLine(idColumn);
+		addText(idColumn, botId);
 	}
 }
 
-function main(){
-	chrome.runtime.sendMessage({
-		showBots: true
-	}, function(_bots) {
-		bots = _bots
-		document.getElementById("stat").innerHTML=`There are ${bots.length} known bot accounts and while we try our best to carefully investigate every user reported, mistakes do happen. Appeals can be made in our Discord server.`
-		document.getElementById("showbots").onclick = function(){
-			document.getElementById("showbots").remove()
-			document.getElementById("botcol1").innerHTML="<b>User ID</b><br>"; 
-			document.getElementById("botcol2").innerHTML="<b>Username</b><br>"
-			addbots(1); //yes i know arrays start at 0, i just don't want cotten eye joe displaying
-		}
-	});
+async function getBotList(){
+	// Pings chrome with {showBots: true} and returns the awaited response
+	return await new Promise(resolve => {
+		chrome.runtime.sendMessage({
+			showBots: true
+		}, function(res) {
+			resolve(res)
+		})
+	})
+}
+
+async function main(){
+	bots = await getBotList();
+
+	// Modify stat paragraph to include the # of bots listed on our gist
+	document.getElementById("stat").firstChild.nodeValue = `There are ${bots.length} known bot accounts and while we try our best to carefully investigate every user reported, mistakes do happen. Appeals can be made in our Discord server.`
+	
+	// Handle clicks for element button `showbots`
+	document.getElementById("showbots").onclick = function(){
+		document.getElementById("showbots").remove()
+		displayBots();
+	}
 }
 
 window.onload = function(){
+	// Get id and username column for bot list on the page
+	idColumn = document.getElementById("botcol1");
+	usernameColumn = document.getElementById("botcol2");
+	
 	main()
 }
 
